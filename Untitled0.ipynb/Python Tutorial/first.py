eddf417d22e74857ae538893d3efd7a53b6e466c
Umar Ansari <umaransari983882@gmail.com>
2026-03-16 10:47:02 +0530

    Created using Colab

--- Untitled0.ipynb/Python Tutorial/first.py
+++ Untitled0.ipynb/Python Tutorial/first.py
@@ -4,7 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyPBaW0VgRWeESMeXSBYSB0m",
+      "authorship_tag": "ABX9TyNtbGvua5wu68Dy+npt/ykk",
       "include_colab_link": true
     },
     "kernelspec": {
@@ -38,14 +38,44 @@
     {
       "cell_type": "code",
       "source": [
-        "from google.colab import drive\n",
-        "drive.mount('/content/drive')"
+        "# to find out hcf\n",
+        "\n",
+        "# input two numbers\n",
+        "num1 = int(input(\"Enter first number: \"))\n",
+        "num2 = int(input(\"Enter second number: \"))\n",
+        "\n",
+        "# -----------------\n",
+        "\n",
+        "if num2 == 0:\n",
+        "    print(\"HCF:\", num1)\n",
+        "\n",
+        "else:\n",
+        "    while num2 != 0:\n",
+        "        rem = num1 % num2\n",
+        "        num1 = num2\n",
+        "        num2 = rem\n",
+        "\n",
+        "    print(\"HCF:\", num1)"
       ],
       "metadata": {
-        "id": "QryC3eEG4NDI"
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "QryC3eEG4NDI",
+        "outputId": "2aef10bc-eddd-4c7c-a31c-ee522619f5d5"
       },
-      "execution_count": null,
-      "outputs": []
+      "execution_count": 8,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Enter first number: 2\n",
+            "Enter second number: 3\n",
+            "HCF: 1\n"
+          ]
+        }
+      ]
     }
   ]
 }
